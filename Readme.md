6d892dd3aa7fc812f538004f89859b6401debfe7
rahul-gurujala <rahul.gurujala@neosoftmail.com>
2021-06-27 04:01:51 +0530

    Made app with routes

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Follow the steps to run django app locally
+`$ virtualenv venv && source venv/bin/activate`
+
+`$ python manage.py migrate`
+
+`$ python manage.py runserver`
---
+++ book_outlet/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from book_outlet.models import Book
+
+# Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'books': books
+    })
+
+
+def book_detail(request, _id):
+    # try: book = Book.objects.get(pk=_id)
+    # except: raise Http404()
+    book = get_object_or_404(Book, pk=_id)
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestselling
+    })
