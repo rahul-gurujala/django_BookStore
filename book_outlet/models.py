f1efb0c4632f8fea5ce7804ee925ed9f1e949d15
rahul-gurujala <rahul.gurujala@neosoftmail.com>
2021-06-28 01:56:54 +0530

    All Done

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,26 +1,64 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import SlugField
 from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return f'{self.name} ({self.code})'
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.street}, {self.city}, {self.postal_code}'
+
+    class Meta:
+        verbose_name_plural = 'Address Entries'
+        pass
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=CASCADE, null=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self) -> str:
+        return self.full_name()
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = SlugField(default='', null=False, db_index=True)
+    slug = SlugField(default='', blank=True,
+                     null=False, db_index=True)
+    published_country = models.ManyToManyField(Country, related_name='books')
+
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.title} ({self.rating})'
